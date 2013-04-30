cf8340f6df5f7800ddcbfedb5a78598d365186d4
caiguanhao <caiguanhao@gmail.com>
2013-04-30 14:48:09 +0800

    add a video to about page;

--- _plugins/liquid_extend.rb
+++ _plugins/liquid_extend.rb
@@ -10,6 +10,9 @@ module LiquidExtend
   def fixnewline(input)
     input.gsub("\n", "").gsub("<link", "\n<link")
   end
+  def prepend_relative(input, relative)
+    (input[0, 7] == "http://" || input[0, 8] == "https://" ? '' : relative) + input
+  end
   
   Liquid::Template.register_filter self
 end
